753c9edd6b8e18e49666186f79debc0e743f0674
harddownloader <serafimdev@gmail.com>
2022-08-22 21:41:03 +0300

    update login page component - set styles for dark theme

--- pages/[channel]/[locale]/account/login.tsx
+++ pages/[channel]/[locale]/account/login.tsx
@@ -1,10 +1,12 @@
 import { useAuth, useAuthState } from "@saleor/sdk";
+import clsx from "clsx";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
 import { useForm } from "react-hook-form";
 import { useIntl } from "react-intl";
 
+import { Button } from "@/components/Button";
 import { messages } from "@/components/translations";
 import { DEMO_MODE } from "@/lib/const";
 import { usePaths } from "@/lib/paths";
@@ -60,13 +62,19 @@ function LoginPage() {
   }
 
   return (
-    <div className="min-h-screen bg-no-repeat bg-cover bg-center bg-gradient-to-r from-blue-100 to-blue-500">
-      <div className="flex justify-end">
-        <div className="bg-white min-h-screen w-1/2 flex justify-center items-center">
+    <div className="min-h-screen bg-no-repeat bg-cover bg-center bg-gradient-to-r from-red-100 to-action-1">
+      <div className="block md:flex md:justify-end">
+        <div
+          className={clsx(
+            "bg-white min-h-screen w-full flex justify-center items-center",
+            "dark:bg-black",
+            "md:w-1/2"
+          )}
+        >
           <div>
             <form onSubmit={handleLogin}>
               <div>
-                <span className="text-sm text-gray-900">
+                <span className="text-sm text-gray-900 dark:text-neutral-300">
                   {t.formatMessage(messages.loginWelcomeMessage)}
                 </span>
                 <h1 className="text-2xl font-bold">{t.formatMessage(messages.loginHeader)}</h1>
@@ -104,12 +112,9 @@ function LoginPage() {
                 </span>
               </div>
               <div className="">
-                <button
-                  type="submit"
-                  className="mt-4 mb-3 w-full bg-green-500 hover:bg-green-400 text-white py-2 rounded-md transition duration-100"
-                >
+                <Button className="mt-4 w-full duration-100">
                   {t.formatMessage(messages.logIn)}
-                </button>
+                </Button>
                 {!!errorsForm.email && (
                   <p className="text-sm text-red-500 pt-2">{errorsForm.email?.message}</p>
                 )}
