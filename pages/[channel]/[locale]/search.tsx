d549baef8c516b06608f22f426c1a4a7fd271525
harddownloader <serafimdev@gmail.com>
2022-09-01 14:53:45 +0300

    update search page - add new styles for search input

--- pages/[channel]/[locale]/search.tsx
+++ pages/[channel]/[locale]/search.tsx
@@ -1,9 +1,11 @@
+import clsx from "clsx";
 import { useQueryState } from "next-usequerystate";
 import React, { ReactElement } from "react";
 import { useIntl } from "react-intl";
 import { useDebounce } from "react-use";
 
 import { Layout, ProductCollection } from "@/components";
+import { getIcon } from "@/components/Navbar/NavIconButton";
 import { messages } from "@/components/translations";
 import { ProductFilterInput } from "@/saleor/api";
 
@@ -25,17 +27,28 @@ function SearchPage() {
   );
 
   return (
-    <main className="container w-full px-8 mt-5">
-      <p className="font-semibold text-xl mb-5">{t.formatMessage(messages.searchHeader)}</p>
-      <input
-        className="w-full md:w-96 mb-10 block border-gray-300 rounded-md shadow-sm text-md"
-        type="text"
-        value={searchQuery || ""}
-        placeholder={t.formatMessage(messages.searchFieldPlaceholder)}
-        onChange={(e) => setSearchQuery(e.target.value, { scroll: false, shallow: true })}
-        data-testid="searchInput"
-      />
-      <ProductCollection filter={debouncedFilter} />
+    <main className="w-full h-auto xl:container">
+      <div className="w-full h-16 mb-6 px-3 md:px-4 flex items-center bg-action-1">
+        <input
+          className={clsx(
+            "w-screen h-full block border-none text-md bg-inherit text-white placeholder:text-slate-200 ring-transparent",
+            "focus:outline-none focus:border-none focus:ring-none focus:ring-transparent"
+          )}
+          type="text"
+          value={searchQuery || ""}
+          placeholder={t.formatMessage(messages.searchFieldPlaceholder)}
+          onChange={(e) => setSearchQuery(e.target.value, { scroll: false, shallow: true })}
+          data-testid="searchInput"
+        />
+        <div
+          className="w-12 xl:w-18 h-full bg-inherit text-white flex items-center justify-center"
+        >
+          {getIcon("spyglass")}
+        </div>
+      </div>
+      <div className="px-8">
+        <ProductCollection filter={debouncedFilter} />
+      </div>
     </main>
   );
 }
